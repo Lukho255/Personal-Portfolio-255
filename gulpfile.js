9922eed53789a71c71cff3168247c584ebeccdfb
Lukho Mazaza <2017157161@ufh.ac.za>
2021-09-24 04:29:59 +0200

    the start of the project and creating new dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
+/* eslint-disable space-before-function-paren */
 const gulp = require('gulp');
 const babel = require('gulp-babel');
-const eslint = require('gulp-eslint')
-gulp.task('default', async function () {
-	// Run ESLint
-	gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
-		.pipe(eslint())
-		.pipe(eslint.format());
-	// Node source
-	gulp.src("es6/**/*.js")
-		.pipe(babel())
-		.pipe(gulp.dest("dist"));
-	// browser source
-	gulp.src("public/es6/**/*.js")
-		.pipe(babel())
-		.pipe(gulp.dest("public/dist"));
+const eslint = require('gulp-eslint');
+
+gulp.task('default', async() => {
+    // Run ESLint
+    gulp.src(['es6/**/*.js', 'public/es6/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format());
+    // Node source
+    gulp.src('es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
+    // browser source
+    gulp.src('public/es6/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('public/dist'));
+    // eslint-disable-next-line eol-last
 });
